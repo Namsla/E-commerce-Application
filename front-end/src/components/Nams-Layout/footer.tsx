6f165fa97f44604050dd154bdeeb2335f2112416
Namkhang Tsamchoe <ntnamkhang@gmail.com>
2023-07-27 22:59:56 -0400

    feat: add scroll bars to the pages

--- front-end/src/components/Nams-Layout/footer.tsx
+++ front-end/src/components/Nams-Layout/footer.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Footer1() {
   return (
-    <footer className="bg-white shadow dark:bg-gray-800">
+    <footer className="bg-amber-100 shadow dark:bg-gray-800">
       <div className="p-4 mx-auto max-w-screen-xl md:flex md:items-center md:justify-between md:p-6">
         <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
           © 2023
